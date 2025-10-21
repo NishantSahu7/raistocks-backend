fcb70693d35de8f3083695e092da964f5afb6ebb
Rukshanda <rukshandanadir@gmail.com>
2025-10-21 21:09:27 +0500

    new-changes

--- src/server.js
+++ src/server.js
@@ -75,7 +75,7 @@ app.use("/api/subscription", subscriptionRoutes);
 app.use("/api/trade-diary", tradeDiaryRoutes);
 app.use("/api/support", supportRoutes);
 app.use("/api/clients", clientRoutes);
-app.use("/api/client/auth", clientAuthRoutes); 
+app.use("/client/auth", clientAuthRoutes); 
 
 
 
