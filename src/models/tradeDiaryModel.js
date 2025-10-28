1d9854e81609c16658a22ba07c5c6ed8291d28bf
Ankit Verma <112801010+ankitverma3490@users.noreply.github.com>
2025-10-28 17:42:58 +0530

    Refactor tradeDiarySchema with new field names

--- src/models/tradeDiaryModel.js
+++ src/models/tradeDiaryModel.js
@@ -1,43 +1,58 @@
 import mongoose from "mongoose";
 
-const tradeDiarySchema = new mongoose.Schema(
-  {
-    crmUser: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "CRM",
-      required: [true, "A trade must belong to a user."],
-    },
-    date: {
-      type: Date,
-     },
-    tradeTitle: {
-      type: String,
-       trim: true,
-    },
-    action: {
-      type: String,
-     },
-    entry: {
-      type: Number,
-     },
-    yourEntry: {
-      type: Number,
-     },
-    exit: {
-      type: Number,
-     },
-    yourExit: {
-      type: Number,
-     },
-    qty: {
-      type: Number,
-     },
-    pnl: { type: Number,  },
-    result: { type: String,  },
+// const tradeDiarySchema = new mongoose.Schema(
+//   {
+//     crmUser: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "CRM",
+//       required: [true, "A trade must belong to a user."],
+//     },
+//     date: {
+//       type: Date,
+//      },
+//     tradeTitle: {
+//       type: String,
+//        trim: true,
+//     },
+//     action: {
+//       type: String,
+//      },
+//     entry: {
+//       type: Number,
+//      },
+//     yourEntry: {
+//       type: Number,
+//      },
+//     exit: {
+//       type: Number,
+//      },
+//     yourExit: {
+//       type: Number,
+//      },
+//     qty: {
+//       type: Number,
+//      },
+//     pnl: { type: Number,  },
+//     result: { type: String,  },
+//   },
+//   { timestamps: true }
+// );
+const tradeDiarySchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "CRM",
+    required: true
   },
-  { timestamps: true }
-);
-
+  trade_title: String,
+  recommended_entry: Number,
+  recommended_exit: Number,
+  entry: Number,
+  exit: Number,
+  quantity: Number,
+  pnl: Number,
+  result: String,
+  date: Date
+});
 const TradeDiary = mongoose.model("TradeDiary", tradeDiarySchema);
 
 export default TradeDiary;
