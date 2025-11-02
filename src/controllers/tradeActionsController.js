2cca4178c713451f6035c297c4f0f3dde45fc1f6
ankitverma3490 <ankitverma3490@gmail.com>
2025-11-03 01:26:55 +0530

    time zone fixd

--- src/controllers/tradeActionsController.js
+++ src/controllers/tradeActionsController.js
@@ -70,7 +70,8 @@ export const addTradeAction = async (req, res) => {
       price,
       comment,
       trialSl,
-      actionDateTime,
+      actionDateTime: new Date(actionDateTime).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }),
+
     });
 
     // 🧠 If type is NOT "update", mark trade Closed and record exit price
